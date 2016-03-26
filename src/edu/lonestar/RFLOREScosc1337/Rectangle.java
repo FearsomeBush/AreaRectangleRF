a1db329d54472783e2d951abc06ec569f9aa0c4b
FearsomeBush <rflores26@my.lonestar.edu>
2016-03-26 14:21:12 -0500

    refactor to add Rectangle

---
+++ src/edu/lonestar/RFLOREScosc1337/Rectangle.java
@@ -0,0 +1,36 @@
+package edu.lonestar.RFLOREScosc1337;
+/**
+ * 
+ * @author Robert
+ *
+ */
+public class Rectangle {
+	/** rectangle width */
+	private double width;
+	/** rectangle length */
+	private double length;
+	
+	/**
+	 * constructor
+	 * @param rectangle width
+	 * @param rectangle length
+	 */
+	 
+	public Rectangle (double width, double length) {
+		this.width = width;
+		this.length = length;
+	}
+	
+	public double getArea(){
+		return .5 * (width * length);
+	}
+	/**
+	 * 
+	 */
+	@Override
+	public String toString(){
+		return String.format("Rectangle width = %f\nRectangle length = %f\nRectangle area = %f",
+				width, length, getArea());
+	}
+	}
+
